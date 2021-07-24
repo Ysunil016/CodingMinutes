7d5260ce06ab04d60e985ecdd1178c2b5d66af21
Sunil Yadav <sunil016@yahoo.com>
2021-07-24 21:43:06 +0530

    sy #5 - String - Sort Subsequence

---
+++ src/String/SortSubsequence.java
@@ -0,0 +1,30 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SortSubsequence {
+  public static void main(String[] args) {
+    String str = "qwerty";
+
+    char[] arr = str.toCharArray();
+    Arrays.sort(arr);
+    str = String.valueOf(arr);
+
+    List<String> res = new ArrayList<>();
+    subset(str, 0, "", res);
+
+    for (String s : res)
+      System.out.println(s);
+  }
+
+  private static void subset(String s, int idx, String res, List<String> result) {
+    if (idx >= s.length()) {
+      result.add(res);
+      return;
+    }
+    subset(s, idx + 1, res, result);
+    subset(s, idx + 1, res + s.charAt(idx), result);
+  }
+}
