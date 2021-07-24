a49cf97dbe86eb5acfff85eb895132bf6596528b
Sunil Yadav <sunil016@yahoo.com>
2021-07-24 09:01:02 +0530

    sy #3 - Array - Mountain - Refactor

--- src/Array/Mountain.java
+++ src/Array/Mountain.java
@@ -4,6 +4,9 @@ public class Mountain {
   public static void main(String[] args) {
     int[] arr = new int[]{5, 6, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0, 1, 2, 3, -2, 4};
     System.out.println(maxPeakLength(arr));
+
+    arr = new int[]{0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0};
+    System.out.println(maxPeakLength(arr));
   }
 
   private static int maxPeakLength(int[] arr) {
@@ -13,18 +16,18 @@ public class Mountain {
     for (int i = 1; i < arr.length - 2; i++) {
       int leftElement = arr[i - 1];
       int rightElement = arr[i + 1];
-      boolean isPeak = arr[i] > leftElement && arr[i] < rightElement;
+      boolean isPeak = arr[i] > leftElement && arr[i] > rightElement;
 
       if (isPeak) {
-        int lengthForPeak = 1;
+        int lengthForPeak = 0;
 
-        int left = i - 1;
-        int right = i + 1;
-        while (left >= 0 && arr[left] < arr[left + 1]) {
+        int left = i;
+        int right = i;
+        while (left > 0 && arr[left] > arr[left - 1]) {
           lengthForPeak++;
           left--;
         }
-        while (right < arr.length && arr[right] > arr[right + 1]) {
+        while (right < arr.length - 1 && arr[right] > arr[right + 1]) {
           lengthForPeak++;
           right++;
         }
