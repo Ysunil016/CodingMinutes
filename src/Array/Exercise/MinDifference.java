28f37947a4ace4637c887aaf596c1c56b98803c3
Sunil Yadav <sunil016@yahoo.com>
2021-07-24 17:05:29 +0530

    sy #E2 - Array - Exercise - Min Difference

---
+++ src/Array/Exercise/MinDifference.java
@@ -0,0 +1,39 @@
+package Array.Exercise;
+
+import java.util.Arrays;
+
+public class MinDifference {
+
+  public static void main(String[] args) {
+    int[] A = new int[]{23, 5, 10, 17, 30};
+    int[] B = new int[]{26, 134, 135, 14, 19};
+    int[] res = minDifference(A, B);
+    System.out.println(res[0] + " " + res[1]);
+  }
+
+  static int[] minDifference(int[] A, int[] B) {
+    // Sorting the Arrays
+    Arrays.sort(A);
+    Arrays.sort(B);
+
+    int min_Difference = Integer.MAX_VALUE;
+    int aPointer = 0;
+    int bPointer = 0;
+    int[] res = new int[]{-1, -1};
+
+    // Traversing Both the Arrays.
+    while (aPointer < A.length && bPointer < B.length) {
+      int difference = Math.abs(A[aPointer] - B[bPointer]);
+
+      if (difference < min_Difference) {
+        res[0] = A[aPointer];
+        res[1] = B[bPointer];
+        min_Difference = difference;
+      }
+
+      if (A[aPointer] < B[bPointer]) aPointer++;
+      else bPointer++;
+    }
+    return res;
+  }
+}
