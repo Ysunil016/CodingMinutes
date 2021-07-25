fb0040f7ece739ec0b2734ba7b959089878b946b
Sunil Yadav <sunil016@yahoo.com>
2021-07-25 16:33:03 +0530

    sy #E5 - String - Exercise - Biggest Number String

---
+++ src/String/Excercise/BiggestNumberString.java
@@ -0,0 +1,32 @@
+package String.Excercise;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BiggestNumberString {
+  public static void main(String[] args) {
+    ArrayList<Integer> nums = new ArrayList<>() {{
+      add(10);
+      add(11);
+      add(20);
+      add(30);
+      add(3);
+    }};
+
+    System.out.println(largest(nums));
+  }
+
+  private static String largest(ArrayList<Integer> nums) {
+    StringBuilder result = new StringBuilder();
+
+    Collections.sort(nums, (a, b) -> {
+      int compare = b.toString().compareTo(a.toString());
+      return compare;
+    });
+
+    for (Integer str : nums)
+      result.append(str);
+
+    return new String(result);
+  }
+}
