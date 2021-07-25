156f13cf34cd92a22d0d2b6ad122c3bb79345666
Sunil Yadav <sunil016@yahoo.com>
2021-07-25 15:39:08 +0530

    sy #E1 - String - Exercise - Digital Clock

---
+++ src/String/Excercise/DigitalClock.java
@@ -0,0 +1,14 @@
+package String.Excercise;
+
+public class DigitalClock {
+
+  public static void main(String[] args) {
+    System.out.println(solve(1180));
+  }
+
+  private static String solve(int minutes) {
+    int hour = minutes / 60;
+    int min = minutes % 60;
+    return hour + ":" + min;
+  }
+}
