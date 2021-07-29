9e95268eab3cc3c54976f2b8c89a49b547703462
Sunil Yadav <sunil016@yahoo.com>
2021-07-29 05:56:52 +0530

    sy #5 - DP_1D - Frog Jump - Refactor

--- src/_16_DynamicProgramming/FrogJump.java
+++ src/_16_DynamicProgramming/FrogJump.java
@@ -63,12 +63,12 @@ public class FrogJump {
   // O(n) - Tabular
   private static int minCost(int[] heights) {
     int dp[] = new int[heights.length + 1];
-    dp[0] = 0;
+    dp[1] = Math.abs(heights[1] - heights[0]);
 
-    for (int i = 1; i < heights.length; i++) {
-      dp[i] = Math.abs(heights[i] - heights[i - 1]) + dp[i - 1];
-      if (i > 1)
-        dp[i] = Math.min(dp[i], Math.abs(heights[i] - heights[i - 2]) + dp[i - 2]);
+    for (int i = 2; i < heights.length; i++) {
+      int oneStep = Math.abs(heights[i] - heights[i - 1]) + dp[i - 1];
+      int twoStep = Math.abs(heights[i] - heights[i - 2]) + dp[i - 2];
+      dp[i] = Math.min(oneStep, twoStep);
     }
 
     return dp[heights.length - 1];
